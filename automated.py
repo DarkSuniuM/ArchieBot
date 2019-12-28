47666f98f44fe69c6692f19f9487d8f48df42c1d
Alireza Ayinmehr <alireza.darksun@gmail.com>
2019-12-28 21:07:29 +0330

    Update automated to use new db module

--- automated.py
+++ automated.py
@@ -3,28 +3,32 @@
 
 import datetime as dt
 
-from models import PendingUser, session
-from app import updater, unrestricted_permissions
+from db.models import PendingUser, session
+from bot import updater, UNRESTRICTED_PERMISSIONS
 
 
 bot = updater.bot
 
 
-def getPendingUsers():
-    now = dt.datetime.utcnow()
-    _30_minutes_ago = now + dt.timedelta(minutes=-1)
-    print(_30_minutes_ago)
+def get_pending_users():
+    """Get pending users.
 
+    Get users who are not activated after 30 minutes.
+    """
+    now = dt.datetime.utcnow()
+    _30_minutes_ago = now + dt.timedelta(minutes=-30)
     users = session.query(PendingUser).filter(PendingUser.create_date < _30_minutes_ago).all()
     return users
 
 
-def unrestrictTemporary(user_tid, group_tid, message_tid):
-    bot.restrictChatMember(group_tid, user_tid, unrestricted_permissions)
+def unrestrict_temporary(user_tid, group_tid, message_tid):
+    """Unrestrict user temporary and delete bot's activation message."""
+    bot.restrictChatMember(group_tid, user_tid, UNRESTRICTED_PERMISSIONS)
     bot.deleteMessage(group_tid, message_tid)
 
 
 if __name__ == "__main__":
-    for pending in getPendingUsers():
-        print(pending)
-        unrestrictTemporary(pending.user.user_tid, pending.user.group_tid, pending.message_tid)
+    for pending in get_pending_users():
+        unrestrict_temporary(pending.user.user_tid,
+                             pending.user.group_tid,
+                             pending.message_tid)
