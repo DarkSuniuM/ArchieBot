ccf05c4415b79ff8baa2da7d4c103ac2917cec8b
Alireza Ayinmehr <alireza.darksun@gmail.com>
2019-12-28 17:22:15 +0330

    Use SQLAlchemy instead of Peewee

--- models.py
+++ models.py
@@ -1,24 +1,42 @@
-import os
+"""
+Models file.
 
-from peewee import SqliteDatabase, Model, CompositeKey
-from peewee import IntegerField, BooleanField
+Database instance and all of application models are defined here!
+"""
 
-from config import BASE_DIR
+import datetime as dt
 
-db = SqliteDatabase(os.path.join(BASE_DIR, 'db.sqlite3'), pragmas={
-    'journal_mode': 'wal',
-    'cache_size': 10000,
-    'foreign_keys': 1,
-    'ignore_check_constraints': 0,
-    'synchronous': 0
-})
+from sqlalchemy.engine import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, Boolean, DateTime, UniqueConstraint
+
+from config import DB_URI
 
 
-class User(Model):
-    user_id = IntegerField(null=False, unique=False)
-    group_id = IntegerField(null=False, unique=False)
-    is_active = BooleanField(null=False, unique=False, default=False)
+db = create_engine(DB_URI)
+Base = declarative_base(db)
+Session = sessionmaker(db)
 
-    class Meta:
-        database = db
-        primary_key = CompositeKey('user_id', 'group_id')
+
+def session():
+    """Create a database session."""
+    return Session()
+
+
+class User(Base):
+    """User Model."""
+
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer,
+                     nullable=False, unique=False, index=True)
+    group_id = Column(Integer,
+                      nullable=False, unique=False, index=True)
+    is_active = Column(Boolean,
+                       nullable=False, unique=False, index=False,
+                       default=False)
+    create_date = Column(DateTime,
+                         nullable=False, unique=False, index=False,
+                         default=dt.datetime.utcnow)
+    __table_args__ = (UniqueConstraint('user_id', 'group_id'), )
