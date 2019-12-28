38ed2bd009f4fe5d24fb881f2028872ac308a737
Alireza Ayinmehr <alireza.darksun@gmail.com>
2019-12-28 20:10:41 +0330

    Add Delete-db

--- archie.py
+++ archie.py
@@ -11,7 +11,14 @@ def generateDatabase():
     Base.metadata.create_all()
 
 
+@click.command('delete-db')
+def deleteDatabase():
+    from models import Base
+    Base.metadata.drop_all()
+
+
 cli.add_command(generateDatabase)
+cli.add_command(deleteDatabase)
 
 if __name__ == "__main__":
     cli()
