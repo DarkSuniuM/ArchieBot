4efb01ea69fb15365bd577ec25fd88fbf5e48867
Alireza Ayinmehr <alireza.darksun@gmail.com>
2019-12-28 17:01:14 +0330

    Add DB_URI config key

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv('BOT_TOKEN')
-PROXY = os.getenv('BOT_PROXY') or None
+PROXY = os.getenv('BOT_PROXY')
+DB_URI = os.getenv('DB_URI')
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
