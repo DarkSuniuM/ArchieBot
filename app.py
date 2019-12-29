70b5924a05c0d367619f6eb7e55a0febcc88168f
Alireza Ayinmehr <alireza.darksun@gmail.com>
2019-12-29 19:14:28 +0330

    Switch logging.info back to print!

--- app.py
+++ app.py
@@ -3,30 +3,30 @@
 import logging
 import config
 
-logging.info("=" * 25)
-logging.info("Loading modules...")
-logging.info("=" * 25)
-logging.info("Loading configurations...")
-logging.info("=" * 25)
-logging.info(f"Path: {config.BASE_DIR}")
-logging.info(f"Database: {config.DB_URI}")
-logging.info(f"Bot Token: {config.TOKEN}")
-logging.info("=" * 25)
+print("=" * 25)
+print("Loading modules...")
+print("=" * 25)
+print("Loading configurations...")
+print("=" * 25)
+print(f"Path: {config.BASE_DIR}")
+print(f"Database: {config.DB_URI}")
+print(f"Bot Token: {config.TOKEN}")
+print("=" * 25)
 
 from bot import updater
-logging.info("Updater Initialized")
-logging.info("=" * 25)
+print("Updater Initialized")
+print("=" * 25)
 
 logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=logging.INFO)
 
-logging.info("Starting polling...")
+print("Starting polling...")
 updater.start_polling()
-logging.info("Polling stared")
-logging.info("=" * 25)
+print("Polling stared")
+print("=" * 25)
 updater.idle()
-logging.info("=" * 25)
-logging.info("Stopping the updater...")
+print("=" * 25)
+print("Stopping the updater...")
 updater.stop()
-logging.info("=" * 25)
+print("=" * 25)
