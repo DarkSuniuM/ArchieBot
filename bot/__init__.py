1c20b3d7a99d435efc293fb13c79a1c7ce6e376a
Alireza Ayinmehr <alireza.darksun@gmail.com>
2019-12-29 19:21:16 +0330

    Register delete_pending_activation_messages to job queue

--- bot/__init__.py
+++ bot/__init__.py
@@ -11,20 +11,19 @@ UNRESTRICTED_PERMISSIONS = ChatPermissions(can_send_messages=True)
 updater = Updater(TOKEN,
                   request_kwargs={'proxy_url': PROXY} if PROXY else None,
                   use_context=True)
+dp = updater.dispatcher
+job_queue = updater.job_queue
 
 
 from .handlers import check_user, kick_bots, check_answer, error_handler
 
-updater.dispatcher.add_handler(
-    MessageHandler(Filters.status_update.new_chat_members, kick_bots)
-)
-updater.dispatcher.add_handler(
-    MessageHandler(Filters.all, check_user)
-)
+dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, kick_bots))
+dp.add_handler(MessageHandler(Filters.all, check_user))
 
+dp.add_handler(CallbackQueryHandler(check_answer))
 
-updater.dispatcher.add_handler(
-    CallbackQueryHandler(check_answer)
-)
-updater.dispatcher.add_error_handler(error_handler)
+dp.add_error_handler(error_handler)
 
+from .jobs import delete_pending_activation_messages
+
+job_queue.run_repeating(delete_pending_activation_messages, 10)
